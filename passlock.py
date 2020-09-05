25b62a1dff90cb948287fd3631acdc291cd8fbde
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-05 19:49:45 +0300

    user credencials save, display and delete

---
+++ passlock.py
@@ -0,0 +1,30 @@
+class User:
+    """
+    Create User class that generates new instances of a user.
+
+    """
+    user_list = []
+
+    def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
+    
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def delete_user(self):
+        '''
+        delete_account method deletes a  saved account from the list
+        '''
+        User.user_list.remove(self)
