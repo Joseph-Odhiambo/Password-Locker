9b0c1b99088167b2bcb30ffb7149a47e3be74c2b
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-05 20:27:09 +0300

    passlock store

--- credentials.py
+++ credentials.py
@@ -1,37 +1,6 @@
 import random
 import string
 import pyperclip
-class User:
-    """
-    Create User class that generates new instances of a user.
-
-    """
-    user_list = []
-
-    def __init__(self, username, password):
-        """
-        method that defines the properties of a user.
-        """
-        self.username = username
-        self.password = password
-
-    def save_user(self):
-        """
-        A method that saves a new user instace into the user list
-        """
-        User.user_list.append(self)
-    
-
-    @classmethod
-    def display_user(cls):
-        return cls.user_list
-
-    def delete_user(self):
-        '''
-        delete_account method deletes a  saved account from the list
-        '''
-        User.user_list.remove(self)
-
 class Credentials():
     """
     Create credentials class to help create new objects of credentials
--- passlock_test.py
+++ passlock_test.py
@@ -1,6 +1,6 @@
 import unittest
 from passlock import User
-# from passlock import Credentials
+from passlock import Credentials
 
 class TestClass(unittest.TestCase):
     """
@@ -26,7 +26,8 @@ class TestClass(unittest.TestCase):
         """
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
-    class TestCredentials(unittest.TestCase):
+
+class TestCredentials(unittest.TestCase):
     """
     A test class that defines test cases for credentials class
 
@@ -78,6 +79,35 @@ class TestClass(unittest.TestCase):
 
         self.new_credential.delete_credentials()
         self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_find_credentialr(self):
+        """
+        test to check if we can find a credential entry by account name and display the details of the credential
+        """
+        self.new_credential.save_details()
+        test_credential = Credentials("Twitter","mikeycharles","Mfh45hfk") 
+        test_credential.save_details()
+
+        the_credential = Credentials.find_credential("Twitter")
+
+        self.assertEqual(the_credential.account,test_credential.account)
+
+    def test_credential_exist(self):
+        """
+        test to check if we can return a true or false based on whether we find or can't find the credential.
+        """
+        self.new_credential.save_details()
+        the_credential = Credentials("Twitter", "mikeycharles", "Mfh45hfk")  
+        the_credential.save_details()
+        credential_is_found = Credentials.if_credential_exist("Twitter")
+        self.assertTrue(credential_is_found)
+
+    def test_display_all_saved_credentials(self):
+        '''
+        method that displays all the credentials that has been saved by the user
+        '''
+
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
 
 if __name__ == "__main__":
     unittest.main()
